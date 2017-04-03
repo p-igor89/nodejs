34ea5948601d891e6d6908aa7cd7db700fb907e0
p-igor89git <iperetyatko@techincubator.com.au>
2017-04-03 22:51:41 +0300

    DEV(chapter1) add second required and test some firures for exports functions and strings

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
-console.log('Hello world');
+console.log('Starting app.js');
 
-const fs = require('fs'),
-      os = require('os');
+const fs    = require('fs');
+const os    = require('os');
+const notes = require('./notes');
 
-var user = os.userInfo();
+//os.userInfo() method returns information about the currently effective user
+let user = os.userInfo(),
+    res  = notes.addNote();
 
-fs.appendFile('firsttest.txt', `Hello ${user.username}!`);
+//Asynchronously append data to a file. The first argument is file name for way and second data
+//Each time we add this text in txt file after run this app.js file
+//`${user.username}` - this is the same like user.username but without "+"
+fs.appendFile('firsttest.txt', `Hello ${user.username}! You ahe is ${notes.age}`, (err) => {
+    if(err){
+        console.log('Enable to write to file');
+    }
+});
+
+//show the function from addNote function
+console.log(res);
+
+//use function which calculate two numbers
+console.log('Result: ' + notes.add(5, -2));
---
+++ notes.js
@@ -0,0 +1,12 @@
+console.log('Starting notes.js');
+
+//create static age data;
+module.exports.age = 27;
+
+module.exports.addNote = () => {
+    return "Return value from addNote function";
+};
+
+module.exports.add = (a, b) => {
+    return a + b;
+};
