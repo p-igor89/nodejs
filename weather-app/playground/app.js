90648416bae2a6293b46ba1dc6292366192fe1be
p-igor89git <iperetyatko@techincubator.com.au>
2017-05-07 17:17:55 +0300

    DEV(weather-app) callbact errors

--- weather-app/playground/app.js
+++ weather-app/playground/app.js
@@ -22,13 +22,19 @@ request({
     url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encoDedAddress}`,
     json: true
 }, (error, response, body)=> {
-    //could write 3 arguments in the first place
-    //console.log(JSON.stringify(body, undefined, 2));
-    //get address
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    //get lag location
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    if(error){
+        console.log('Unable to connect to Goggle servers');
+    }else if(body.status === 'ZERO_RESULTS') {
+        console.log('Unable to find that address');
+    }else if(body.status === 'OK'){
+        //could write 3 arguments in the first place
+        //console.log(JSON.stringify(body, undefined, 2));
+        //get address
+        console.log(`Address: ${body.results[0].formatted_address}`);
+        //get lag location
+        console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+        console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    }
 });
 
 //encodeURIComponent(str) - help change the empty space for %20 - helpful for creating URL
